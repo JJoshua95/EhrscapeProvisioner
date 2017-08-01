2728551f5925fb89487114c9355cbf769725a82c
JJoshua95 <jcjoshua@Jarrods-MacBook-Pro.local>
2017-08-01 18:53:01 +0100

    AngularJS Frontend provisioner functional

--- src/main/webapp/angular/controller/MainController.js
+++ src/main/webapp/angular/controller/MainController.js
@@ -2,6 +2,7 @@ app.controller('MainController', ['$scope', '$window', '$http', function($scope,
 	
 	$scope.username = '';
 	$scope.password = '';
+	$scope.baseUrl = '';
 	
 	$scope.testFunction = function() {
 		$window.alert("Clicked!");
@@ -19,9 +20,24 @@ app.controller('MainController', ['$scope', '$window', '$http', function($scope,
 	$scope.provisionSingle = function() {
 		console.log($scope.username);
 		console.log($scope.password);
-		var body = JSON.stringify({username: $scope.username, password:$scope.password})
+		console.log($scope.baseUrl);
+		var body = JSON.stringify({username: $scope.username, password:$scope.password, baseUrl: $scope.baseUrl})
 		console.log(body);
 		$http.post("http://localhost:8080/EhrscapeProvisioner/api/provision/single-provision", body)
+		.then(function(response) {
+            var res = JSON.stringify(response);
+            //$window.alert(res);
+            console.log(res);
+        });
+	};
+	
+	$scope.provisionSingleWithDemographic = function() {
+		console.log($scope.username);
+		console.log($scope.password);
+		console.log($scope.baseUrl);
+		var body = JSON.stringify({username: $scope.username, password:$scope.password, baseUrl: $scope.baseUrl})
+		console.log(body);
+		$http.post("http://localhost:8080/EhrscapeProvisioner/api/provision/single-provision-with-demographic", body)
 		.then(function(response) {
             var res = JSON.stringify(response);
             //$window.alert(res);
