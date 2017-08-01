4031e84b1322837e9f98e420ad912b2c5c243654
JJoshua95 <jcjoshua@Jarrods-MacBook-Pro.local>
2017-08-01 11:49:45 +0100

    ignore files attempt 1

---
+++ src/main/java/ehrscapeProvisioner/SinglePatient.java
@@ -0,0 +1,63 @@
+package ehrscapeProvisioner;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.http.client.ClientProtocolException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import ehrscapeProvisioner.model.EhrscapeRequest;
+
+/**
+ * Root resource (exposed at "provision" path)
+ */
+@Path("provision")
+public class SinglePatient {
+	
+	@GET
+	@Path("test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String tester() {
+		String str = "Provision";
+		return str;
+	}
+	
+	@POST
+	@Path("single-provision")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String singleProvision(String inputBody) throws ClientProtocolException, IOException, URISyntaxException {
+		EhrscapeRequest req =  new EhrscapeRequest();
+		
+		Gson gson = new Gson();
+		JsonObject jsonInput = (new JsonParser()).parse(inputBody.toString()).getAsJsonObject();
+		System.out.println(jsonInput.get("username").getAsString());
+		System.out.println(jsonInput.get("password").getAsString());
+		
+		String getSessionResponse = req.getSession(jsonInput.get("username").getAsString(),jsonInput.get("password").getAsString()); 
+		String createEhrResponse = req.createEhr(req.config.getSessionId().replace("\"", ""), "uk.nhs.nhs_number", "JarrodEhrscapeProvisioner");
+		String uploadTemplateResponse = req.uploadDefaultTemplate();
+		String uploadCompResponse = req.uploadDefaultComposition();
+		
+		// put the final response stuff here
+		
+		JsonObject jsonOutput = new JsonObject();
+		jsonOutput.addProperty("num", 123);
+		jsonOutput.addProperty("testKey", "testVal"); // for a custom response later if needed
+		
+		String finalConfig = gson.toJson(req.config);
+		//System.out.println(jsonInput.toString());
+		return finalConfig; //gson.toJson(jsonOutput);
+	}
+	
+}
---
+++ src/main/java/ehrscapeProvisioner/model/EhrscapeConfig.java
@@ -0,0 +1,116 @@
+package ehrscapeProvisioner.model;
+
+public class EhrscapeConfig {
+	
+	private String patientsFile = "patients.csv";
+	private String baseUrl = "https://ehrscape.code4health.org/rest/v1/"; // "https://cdr.code4health.org/rest/v1/"; //
+	private String username = "";
+	private String password = "";
+	private String sessionId = "";
+	private String subjectId = "";
+	private String ehrId = "";
+	private String templateId = "Vital Signs Encounter (Composition)";
+	private String compositionId = "";
+	private String commiterName = "ehrscapeProvisioner";
+	
+	public String getPatientsFile() {
+		return patientsFile;
+	}
+	
+	public void setPatientsFile(String patientsFile) {
+		this.patientsFile = patientsFile;
+	}
+	
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+	
+	public void setBaseUrl(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getSessionId() {
+		return sessionId;
+	}
+	
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	
+	public String getSubjectId() {
+		return subjectId;
+	}
+	
+	public void setSubjectId(String subjectId) {
+		this.subjectId = subjectId;
+	}
+	
+	public String getEhrId() {
+		return ehrId;
+	}
+	
+	public void setEhrId(String ehrId) {
+		this.ehrId = ehrId;
+	}
+	
+	public String getTemplateId() {
+		return templateId;
+	}
+	
+	public void setTemplateId(String templateId) {
+		this.templateId = templateId;
+	}
+	
+	public String getCompositionId() {
+		return compositionId;
+	}
+	
+	public void setCompositionId(String compositionId) {
+		this.compositionId = compositionId;
+	}
+	
+	public String getCommiterName() {
+		return commiterName;
+	}
+	
+	public void setCommiterName(String commiterName) {
+		this.commiterName = commiterName;
+	}
+	
+}
+
+/*
+
+var EhrscapeConfig = {
+  patientsFile: 'patients1.csv',
+  baseUrl: 'https://ehrscape.code-4-health.org/rest/v1/',
+  username: '',
+  password: '',
+  sessionId: '',
+  subjectNamespace: 'uk.nhs.hospital_number',
+  subjectId: '',
+  ehrId: '',
+  templateId: 'Vital Signs Encounter (Composition)',
+  compositionId: '',
+  commiterName: 'ehrscapeProvisioner'
+}
+
+module.exports = EhrscapeConfig;
+
+*/
